93e94d76e50a45fa0b0baeb6d0354a4fb5f0310f
anousoneFS <sone.freestyle7898@gmail.com>
2021-10-02 15:51:34 +0700

    remove firebase

--- src/main.js
+++ src/main.js
@@ -11,13 +11,13 @@ import firebase from "firebase"
 // firebase.initializeApp(settings.firebaseConfig);
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDmrWTg3ty-Hhj4dRZy1SEhfrELzzDmgmc",
-  authDomain: "kafareup-66a9d.firebaseapp.com",
-  databaseURL: "https://kafareup-66a9d.firebaseio.com",
-  projectId: "kafareup-66a9d",
-  storageBucket: "kafareup-66a9d.appspot.com",
-  messagingSenderId: "920714222199",
-  appId: "1:920714222199:web:07c1875d589070c3f5c472",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 })
 
 Vue.config.productionTip = false
